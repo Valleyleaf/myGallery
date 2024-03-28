dc56c407cedf9a2b2ca6ac98c045fa86b6893003
Aston Dahllöf <aston_dahllof@outlook.com>
2024-03-28 18:53:44 -0300

    Minor css changes

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -16,11 +16,11 @@ export default function Home(){
                 <h2 className="artTitleWhite">Post-Mortem</h2> 
                 </div>
                 <p className="artTitleWhite">{aboutMe.aboutMe}</p>
-                    {/* <div className="flex-container-row">
+                    <div className="flex-container-row">
                         <div className="flex-container-column homeSide">
                         <div className="flex-container-row center-content">
                             <h2 className="centerFlavorTitle">//</h2> 
-                            <h2 className="articleText">About.</h2>
+                            <h2 className="articleText">Who am I?</h2>
                             <h2 className="centerFlavorTitle">//</h2> 
                             </div>
                             <p className="articleText">{aboutMe.aboutMe2}</p>
@@ -33,7 +33,7 @@ export default function Home(){
                             <img className="profileImage profile2" src="src\assets\img\Profile\Profile_0002_Head.png" alt="ImageofRatMaestro" />
                             <img className="profileImage profile3" src="src\assets\img\Profile\Profile_0003_Layer-1.png" alt="ImageofRatMaestro" />
                         </div>
-                    </div> */}
+                    </div>
             </div>
         </div>
     );
