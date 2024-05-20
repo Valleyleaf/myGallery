80c686a8cc88a142f9ca62293a4be09860502cba
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-05-20 15:12:55 -0300

    Removing legacy code

--- Client/src/assets/js/Gallery.js
+++ Client/src/assets/js/Gallery.js
@@ -1,40 +1,3 @@
-let slideImage = [
-    'https://cdn.discordapp.com/attachments/643686770482872335/1214688804548386886/IMG_20240217_173602_069.jpg?ex=660c7b1a&is=65fa061a&hm=692bc1f666e505438c45bdae4dfc6252c6ddbece42e1cbc42f67d94e4d34468d&',
-    'https://pbs.twimg.com/media/FylVEZEXoAIDkme?format=jpg&name=large',
-    'https://cdn.discordapp.com/attachments/643686770482872335/1221521655520563302/20230724_222035.jpg?ex=6612e1b1&is=66006cb1&hm=b8c0a4a5ed2cf9c6ffdc371fd56208fc93d4102cf7ef97eea5c61e37f3ab479f&',
-    'https://cdn.discordapp.com/attachments/643686770482872335/1214690993908219944/IMG_20230311_191633_001.jpg?ex=6615b7a4&is=660342a4&hm=006de2c01408dfc2531539b8fa9842039f96c59ca5caec892c0667d19cd83601&',
-    'https://cdn.discordapp.com/attachments/643686770482872335/1214688806209458186/IMG_20240217_172913_863.jpg?ex=660c7b1b&is=65fa061b&hm=101028d1e976ccf404c94a4400e7d05bbeac6b077990a9bc57ac42f563ff372b&',
-    'https://cdn.discordapp.com/attachments/643686770482872335/1214688803726426122/20240221_172841.jpg?ex=660c7b1a&is=65fa061a&hm=7c3c8fce87e25cb7ffbb58f9fceedd02908eb78c125cd4ac5b2807187ceb69b7&',
-]
-
-
-
-
-let slideTitle = [
-    'The Herald',
-    'Ghost',
-    'Chance Encounter',
-    'King of Nothing',
-    'Stained',
-    'Panel 01',
-    'Lost in Thought',
-    'Beast',
-    'Sand',
-]
-
-let slideContent = [
-    'Acrylic on 90x30cm canvas. Frame. Signed.',
-    'Acrylic on 20x20cm canvas. Frame. Signed',
-    'Acrylic on 90x30cm canvas. Frame. Signed.',
-    'Acrylic on 20x20cm canvas. Frame. Signed',
-    'Acrylic on 90x30cm canvas. No Frame. Signed',
-    'Acrylic on 90x30cm canvas. No Frame. Signed',
-    'Content 6',
-    'Content 7',
-    'Content 8',
-    
-]
-
 let GalleryData = [
     {
         id: 1,
@@ -42,7 +5,7 @@ let GalleryData = [
         description: 'Acrylic on 90x30cm canvas. Frame. Signed.',
         image: 'https://i.imgur.com/ob6PMkD.jpg',
         cost: '$55',
-        Availability: 'Sold',
+        Availability: 'Available',
         alt: 'Image of Artwork: The Herald'
     },
     {
